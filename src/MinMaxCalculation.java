d39e6996e93ecf7613d72b40edc241e3ede63ff8
Atharva Naik <atharvashrik@umass.edu>
2023-10-01 21:06:41 -0400

    Added MinMaxCaluculation.java

---
+++ src/MinMaxCalculation.java
@@ -0,0 +1,8 @@
+public class MinMaxCalculation{
+    public double calculateMax(ArrayList<Double> data) {
+        return Collections.max(data);
+    }
+    public double calculateMin(ArrayList<Double> data) {
+        return Collections.min(data);
+    }
+}
